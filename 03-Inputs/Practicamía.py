7468be225579a2f0da6f87cc72585387019c379f
isracm357 <isracaicedomorejon@gmail.com>
2024-05-28 23:16:01 -0500

    Práctica propia

---
+++ 03-Inputs/Practicamía.py
@@ -0,0 +1,10 @@
+"""
+Vas a crear un código en Python que permita preguntar a tu 
+amigo estas 2 preguntas:
+¿Que ciudad te gustaria visitar?
+¿Cual es tu color favorito?
+y que luego le muestre en pantalla 
+esas palabras combinadas, para formar una marca creativa.  
+"""
+
+print("Tu marca creativa \nEs: " + (input("¿Qué ciudad te gustaría visitar?: ")) + " "+ (input("¿Cuál es tu color favorito?: "))+"\n¡¡FELICITACIONES!! \n¡¡LO LOGRASTE!!!")
