8f059cc1ee1ef4b8e1e82c7d83d75b648154bb8d
isracm357 <isracaicedomorejon@gmail.com>
2024-05-29 20:00:57 -0500

    Variables

---
+++ 04-Variables/app.py
@@ -0,0 +1,9 @@
+nombre = "Juan"
+#print(nombre)
+nombre="Laura"
+print(nombre)
+
+edad1=30
+edad2=60
+edadtotal=(30+60)
+print(edadtotal)
---
+++ 04-Variables/practica1.py
@@ -0,0 +1,11 @@
+"""
+PRACTICA 1
+Declara dos variables, llamdas nombre y edad.
+Asigna a la variable nombre el valor "Tony", y la edad , el valor 51.
+"""
+
+nombre="Tony"
+print(nombre)
+Edad=51
+print(Edad)
+
