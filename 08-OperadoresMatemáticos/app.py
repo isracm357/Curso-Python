5bed88a6b134de43ff8686dd8b266e428337e6a6
isracm357 <isracaicedomorejon@gmail.com>
2024-05-29 22:27:02 -0500

    Operaciones Matemáticas

---
+++ 08-OperadoresMatemáticos/app.py
@@ -0,0 +1,26 @@
+x = 6
+y = 2
+z = 7
+print(f"{x} + {y} es igual {x+y}")
+print(f"{x} - {y} es igual {x-y}")
+print(f"{x} * {y} es igual {x*y}")
+print(f"{x} / {y} es igual {x/y}")
+
+
+print(f"{z} // {y} es igual {z//y}")
+print(f"{z} % {y} es igual {z%y}")
+
+print(f"{x} elevado a la {y} es igual {x**y}")
+print(f"{x} elevado a la {3} es igual {x**3}")
+
+print(f"La raíz cuadrada de {x} es igual a {x**0.5}")
+
+#REDONDEO 
+
+resultado=97/7
+print(resultado)
+redondeo=round(resultado)
+print(redondeo)
+
+redondeo2=round(resultado,2)
+print(redondeo2)
---
+++ 08-OperadoresMatemáticos/practica2.py
@@ -0,0 +1,14 @@
+"""
+Practica 3
+Calcula la raíz cuadrada de 5, y muestra en 
+pantalla el resultado redondeado con 4 posiciones decimales.
+"""
+
+
+resultado=5**0.5
+redondeo=round(resultado, 4)
+
+print(redondeo)
+
+print(f"La raíz cuadrada de {5} es igual a {redondeo}")
+
