ea259b4ed953c818f1df2767508251d62064d0aa
isracm357 <isracaicedomorejon@gmail.com>
2024-05-30 22:25:36 -0500

    Aprendizaje de Slicing

---
+++ 10-Slicing/practica2.py
@@ -0,0 +1,10 @@
+""" 
+Practica 2
+Toma cada tercer caracter empezando desde el noveno 
+hasta el final de la frase, e imprime el resultado.
+"""
+"Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+
+texto="Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+
+print(texto[9::3])
