704161d264a4b42138e588603bd369482d64ff73
isracm357 <isracaicedomorejon@gmail.com>
2024-06-04 22:45:21 -0500

    Rangos

---
+++ 24-Range/app.py
@@ -0,0 +1,17 @@
+#FORMA TRADICIONAL 
+
+lista = [1,2,3,4]
+
+for num in lista:
+   print(num)
+    
+#CODIGO OPTIMO
+print("\n")
+
+for num in range(1,5):
+    print(num)
+  
+print("\n")  
+#CREACION
+lista = list(range(1,101, 2))
+print(lista)
